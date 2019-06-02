19d8bed6ef43abff4757a770e5a17ca263a6bbb8
ShiboDuan <47977252+ShiboDuan@users.noreply.github.com>
2019-06-02 16:16:41 +0800

    Update ManagerController.java

--- src/main/java/websale/sale/controller/ManagerController.java
+++ src/main/java/websale/sale/controller/ManagerController.java
@@ -75,8 +75,8 @@ public class ManagerController {
 
         if (file==null) System.out.println("file error");
         String path=ImageUtils.saveFile(file,request);
-        //item.setImagePath(path);
-        //managerService.createItem(storeId,item,number);
+        item.setImagePath(path);
+        managerService.createItem(storeId,item,number);
         return "index";
     }
 

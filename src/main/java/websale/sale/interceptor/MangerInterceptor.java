c1dde366e352ef6b1e56e9cd29a51b10afb71976
ShiboDuan <47977252+ShiboDuan@users.noreply.github.com>
2019-06-03 20:05:32 +0800

    mangerInterceptor

---
+++ src/main/java/websale/sale/interceptor/MangerInterceptor.java
@@ -0,0 +1,16 @@
+package websale.sale.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MangerInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        if (request.getSession().getAttribute("mid")!=null){
+            return true;
+        }
+        return false;
+    }
+}

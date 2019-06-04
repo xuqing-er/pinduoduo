a052a71c4e174bce5532c503cb37d23f1b4b04f5
ShiboDuan <47977252+ShiboDuan@users.noreply.github.com>
2019-06-04 13:16:48 +0800

    @configuration

--- src/main/java/websale/sale/configuration/WebConfiguration.java
+++ src/main/java/websale/sale/configuration/WebConfiguration.java
@@ -2,11 +2,13 @@ package websale.sale.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import websale.sale.interceptor.LoginInterceptor;
 import websale.sale.interceptor.MangerInterceptor;
 
+@Configuration
 public class WebConfiguration implements WebMvcConfigurer {
     @Autowired
     private LoginInterceptor loginInterceptor;
